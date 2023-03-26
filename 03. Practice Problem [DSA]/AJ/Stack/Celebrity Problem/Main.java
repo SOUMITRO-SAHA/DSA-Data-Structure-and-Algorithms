dbf7a59725ede0595c65df0d7cf60bbae92f1344
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-03-26 12:36:09 +0530

    Infix Evaluation & Conversions Solved

--- 03. Practice Problem [DSA]/AJ/Stack/Celebrity Problem/Main.java
+++ 03. Practice Problem [DSA]/AJ/Stack/Celebrity Problem/Main.java
@@ -1,60 +1,170 @@
 import java.io.*;
-import java.util.*; 
+import java.util.*;
 
-class Main{
-    public static void main(String args[]) throws IOException { 
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M[][] = new int[N][N];
-        for(int i=0; i<N; i++)
-            for(int j=0; j<N; j++)
-                M[i][j] = sc.nextInt();
-        System.out.println(new Solution().findCelebrity(M,N));
-    } 
-} 
-class Solution{ 
-    int findCelebrity(int M[][], int n){
-		// Traversing Row Wise means, Whom I know.
-		// Traversing Colum Wise means, Who Knows me.
-	    Stack<Integer> st = new Stack<>();
-		for(int i = 0; i < n; i++){
-			st.push(i);
-		}
-		
-		while(st.size() > 1){
-			int p1 = st.pop();
-			int p2 = st.pop();
-			if(M[p1][p2] == 1){
-				// That, means p1 knows p2
-				// Therefore, Eliminate p1
-				st.push(p2);
-			}
-			else{
-				st.push(p1);
-			}
-		}
+public class Main {
 
-		int pCeleb = st.pop();
-		
-		// Checking Whether pCeleb is actually a Celeb or not
-		// I don't know anyone
-		// First, Traversing through Column Wise:
-        for (int c = 0; c < n; c++) {
-            if (M[pCeleb][c] == 1) {
-                return -1;
-            }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String exp = br.readLine();
+        Solution ob = new Solution();
+        ob.evaluate(exp);
+    }
+}
+
+class Solution {
+
+    public int getPrecedence(char ch) {
+        if (ch == '+' || ch == '-') {
+            return 1;
+        } else if (ch == '*' || ch == '/') {
+            return 2;
         }
 
-        // I should be known by everyone
-		// Second, Traversing through row wise:
-        for (int r = 0; r < n; r++) {
-            if (pCeleb != r) {
-                if (M[r][pCeleb] == 0) {
-                    return -1;
+        return -1;
+    }
+
+    // v1 o v2
+    public int eval(int v1, int v2, char o) {
+        if (o == '+') {
+            return v1 + v2;
+        } else if (o == '-') {
+            return v1 - v2;
+        } else if (o == '*') {
+            return v1 * v2;
+        } else if (o == '/') {
+            return v1 / v2;
+        }
+
+        return -1;
+    }
+
+    void solve(String exp) {
+        // 2 stacks: first for opd, second for opr
+        Stack<Character> opr = new Stack<Character>();
+        Stack<Integer> opd = new Stack<Integer>();
+
+        for (int i = 0; i < exp.length(); i++) {
+            char ch = exp.charAt(i);
+
+            if (ch == '(') {
+                opr.push(ch);
+            } else if (Character.isDigit(ch) == true) {
+                opd.push(ch - '0');
+            } else if (ch == ')') {
+                while (opr.peek() != '(') {
+                    char o = opr.pop();
+                    int v2 = opd.pop();
+                    int v1 = opd.pop();
+
+                    int ans = eval(v1, v2, o);
+
+                    opd.push(ans);
                 }
+
+                opr.pop();
+            } else {
+                while (opr.size() > 0 && opr.peek() != '(' && getPrecedence(ch) <= getPrecedence(opr.peek())) {
+                    char o = opr.pop();
+                    int v2 = opd.pop();
+                    int v1 = opd.pop();
+
+                    int ans = eval(v1, v2, o);
+
+                    opd.push(ans);
+                }
+
+                opr.push(ch);
             }
         }
-		
-		return pCeleb;
+
+        while (opr.size() > 0) {
+            char o = opr.pop();
+            int v2 = opd.pop();
+            int v1 = opd.pop();
+
+            int ans = eval(v1, v2, o);
+
+            opd.push(ans);
+        }
+
+        System.out.println(opd.peek());
+    }
+
+    void convert(String exp) {
+        Stack<Character> opr = new Stack<Character>();
+
+        Stack<String> prefix = new Stack<>();
+        Stack<String> postfix = new Stack<>();
+
+        for (int i = 0; i < exp.length(); i++) {
+            char ch = exp.charAt(i);
+
+            if (ch == '(') {
+                opr.push(ch);
+            } else if (Character.isDigit(ch) == true) {
+                prefix.push(ch + "");
+                postfix.push(ch + "");
+            } else if (ch == ')') {
+                while (opr.peek() != '(') {
+                    char o = opr.pop();
+                    String v2 = prefix.pop();
+                    String v1 = prefix.pop();
+
+                    String ans = o + v1 + v2;
+                    prefix.push(ans);
+
+                    String b2 = postfix.pop();
+                    String b1 = postfix.pop();
+                    ans = b1 + b2 + o;
+
+                    postfix.push(ans);
+                }
+
+                opr.pop();
+            } else {
+                while (opr.size() > 0 && opr.peek() != '(' && getPrecedence(ch) <= getPrecedence(opr.peek())) {
+                    char o = opr.pop();
+                    String v2 = prefix.pop();
+                    String v1 = prefix.pop();
+
+                    String ans = o + v1 + v2;
+                    prefix.push(ans);
+
+                    String b2 = postfix.pop();
+                    String b1 = postfix.pop();
+                    ans = b1 + b2 + o;
+
+                    postfix.push(ans);
+                }
+
+                opr.push(ch);
+            }
+        }
+
+        while (opr.size() > 0) {
+            char o = opr.pop();
+            String v2 = prefix.pop();
+            String v1 = prefix.pop();
+
+            String ans = o + v1 + v2;
+            prefix.push(ans);
+
+            String b2 = postfix.pop();
+            String b1 = postfix.pop();
+            ans = b1 + b2 + o;
+
+            postfix.push(ans);
+        }
+
+        System.out.println(postfix.peek());
+        System.out.println(prefix.peek());
+    }
+
+    // TC: O(N), SC: O(N)
+    public void evaluate(String exp) {
+        // Write code here
+        solve(exp);
+
+        convert(exp);
     }
 }
